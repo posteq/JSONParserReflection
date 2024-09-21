1ea1cece0fcf82f19b8cd6d8b4ef48d8fdbc1d90
laptopAsus <home@>
2024-09-22 00:28:31 +0300

    add parser

--- src/main/java/by/clevertec/Main.java
+++ src/main/java/by/clevertec/Main.java
@@ -4,6 +4,7 @@ package by.clevertec;
 import by.clevertec.domain.Customer;
 import by.clevertec.helper.CreatorStructure;
 import by.clevertec.util.JsonSerializers;
+import by.clevertec.util.Parser;
 
 
 public class Main {
@@ -13,6 +14,10 @@ public class Main {
         JsonSerializers serializer = new JsonSerializers();
         String json = serializer.toJson(customer);
         System.out.println("Serialized JSON: " + json);
+
+        Parser parser = new Parser();
+        Object parse = parser.parse(json);
+        System.out.printf("parse string result : \n" + parse.toString());
 
     }
 }
